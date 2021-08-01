fc363307d08ea3f345a972f27f4467bf1b1723ed
Svetoslav Pehlivanov <Svetoslav.Pehlivanov.consultant@cconsulting.it>
2021-08-01 14:17:26 +0300

    ReactApp

---
+++ src/Test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import App from './App';
+import ReactDOM from 'react-dom';
+
+
+
+
+ReactDOM.render(
+<React.StrictMode>
+    <App />
+</React.StrictMode>,
+document.getElementById('root')
+
+);
+function Test(){
+return(
+
+<div className="App">
+    <header className="App-header">
+<div className="App-logo">
+
+
+    KO
+</div>
+
+    </header>
+</div>
+
+);
+
+
+}
+
+export default Test;
